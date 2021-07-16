a26e2aca8201a061a446dc1ceca33b858fe23805
Nkully123 <inonkululekosibiya@gmail.com>
2021-07-16 22:00:12 +0200

    Pushing Project files.

---
+++ src/ca/DoWhilePassword/terminal/do_whilePassword.java
@@ -0,0 +1,39 @@
+
+package ca.DoWhilePassword.terminal;
+import javax.swing.*;
+class Password
+{
+    String choice = "John";
+    public static void main(String[] args) {
+
+    }
+
+    public boolean checkPassword()
+    {
+        String input;
+        boolean correctPassword;
+        int attempts = 1;
+        do
+        {
+            input = JOptionPane.showInputDialog("Enter Password in a password");
+            correctPassword = input.equals(choice);
+            if (!correctPassword)
+            {
+                attempts++;
+                JOptionPane.showMessageDialog(null, "Incorrect password! Please Enter Password");
+            }
+        }
+        while (!correctPassword && attempts <= 3);
+        if (correctPassword)
+        {
+            return true;
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null,
+                    "Sorry, maximum number of attempts exceeded! the current password is " + choice);
+            return false;
+        }
+    }
+}
+
